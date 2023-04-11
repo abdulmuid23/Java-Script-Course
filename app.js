d288b95f7c2f0e70b64858d1105fb3584593bf1b
abdulmuid23 <130435385+abdulmuid23@users.noreply.github.com>
2023-04-11 11:47:17 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+//           Q1            ///
+
+// let x=+prompt("Enter Value you want to Add in 5");
+// function AddNo(value){
+
+// return function(ghi){
+//     console.log(ghi + value)
+// }
+// }
+
+// let innerfunction = AddNo(x)
+// innerfunction(5)
+
+///           Q2            ///
+
+let arr =[1,2,3,4,5,6,7,8,9]
+function search(num,time){
+    if(time>0)
+    {
+        let x=
+        console.log("true")
+    }
+    
+
+}
